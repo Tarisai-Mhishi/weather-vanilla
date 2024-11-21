cd9a1b5d19681ca1eaa67d9bd8375554172ab3cf
Tarisai-Mhishi <tarisaimhishi1@gmail.com>
2024-11-21 13:00:11 +0000

    adding functionality using an API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = response.data.temperature.current;
+}
+
+function searchCity(city) {
+  let apiKey = "a13o8f90df02a42a5dftc6d4450dbe0c";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearch(event) {
   event.preventDefault();
 
   let searchInput = document.querySelector("#search-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search");
 searchFormElement.addEventListener("submit", handleSearch);
