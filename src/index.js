53543173e6836318f2cca541cde6c5de6040a964
Tarisai-Mhishi <tarisaimhishi1@gmail.com>
2024-11-21 12:10:22 +0000

    add basic search engine

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function handleSearch(event) {
+  event.preventDefault();
+
+  let searchInput = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+let searchFormElement = document.querySelector("#search");
+searchFormElement.addEventListener("submit", handleSearch);
